8a99b630c0af5b5859df1365ba075c17555a5573
HN <hni1124@naver.com>
2018-08-29 15:00:59 +0900

    Connect DB, session

--- run.py
+++ run.py
@@ -1,49 +1,70 @@
 from flask import Flask, render_template, request, json, session
+from flaskext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask('__name__')
+app.secret_key = 'flask app secret key'
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
+app.config['MYSQL_DATABASE_DB'] = 'flask_test'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
 
 @app.route('/')
 def login():
+    session.clear()
     return render_template('Login.html')
 
-@app.route('/Layout')
-def layout():
-    return render_template('Layout.html')
 
 @app.route('/Home')
-def Home():
-    return render_template('Home.html')
+def home():
+    return check_valid('Home.html')
+
 
 @app.route('/About')
-def About():
-    return render_template('About.html')
+def about():
+    return check_valid('About.html')
+
 
 @app.route('/Contact')
-def Contact():
-    return render_template('Contact.html')
+def contact():
+    return check_valid('Contact.html')
+
 
 @app.route('/loginProcess', methods=['POST'])
-def loginProcess():
-    tmp_account = {'id': 'qwer', 'pw': '1234'}
-
+def login_process():
     _id = request.form['inputID']
     _pw = request.form['inputPassword']
 
-    if _id == tmp_account['id'] and _pw == tmp_account['pw']:
-        result = 'Success'
-        # session['LoginID'] = request.form['inputID']
-        # session['LoginPW'] = request.form['inputPassword']
-    elif _id == tmp_account['id'] and _pw != tmp_account['pw']:
-        result = 'NotMatchPW'
-    elif _id != tmp_account['id'] and _pw == tmp_account['pw']:
-        result = 'NotMatchID'
+    data = login_db_conn(_id, _pw)
+
+    if len(data) > 0:
+        result = "Success"
+        session['LoginID'] = data[0][0]
+        session['LoginPW'] = data[0][1]
     else:
-        result = 'Fail'
+        result = "Fail"
 
-    return json.dumps(result)
-
-
+    return result
     # return json.dumps({ 'id': _id, 'pw': _pw })
+
+
+def check_valid(url):
+    if session.get('LoginID'):
+        return render_template(url)
+    else:
+        return render_template('Login.html')
+
+
+def login_db_conn(_id, _pw):
+    con = mysql.connect()
+    cursor = con.cursor()
+    cursor.callproc('sp_LoginProcess', (_id, _pw))
+    data = cursor.fetchall()
+    return data
+
 
 if __name__ == '__main__':
     app.debug = True
--- static/js/login.js
+++ static/js/login.js
@@ -5,21 +5,21 @@ $(function() {
             data: $('form').serialize(),
             type: 'POST',
             success: function(response) {
-//                if response == "Success":
-//                    window.location.href = 'Home'
-//                else if response == "NotMatchPW":
-//                    Alert('Does not match the Password!')
-//                else if response == "NotMatchID":
-//                    Alert('Does not match the ID!')
-//                else if response == "Fail":
-//                    Alert('Does not match ID and Password!')
-
-                console.log(response)
-                window.location.href = 'Home'
+                if(response == "Success"){
+                    window.location.href = 'Home';
+                }else{
+                    alert('Fail to Login!');
+                }
             },
             error: function(error) {
                 console.log(error);
             }
         });
+    });
+    $('#inputPassword').keypress(function (e) {
+        var key_ENTER = 13;
+        if (e.keyCode == key_ENTER) {
+            $('#btnLogin').trigger('click');
+        }
     });
 });
